1a212612237e3828c4e6fc1d53e5725a87c885b0
mgalves <mgalves@b816d57c-f52a-0410-81bc-3136d98e5e67>
2007-04-01 17:51:55 +0000

    correcao de erros gerados na versao compacatada. Execucao mais simples.

--- trunk/jscompact.py
+++ trunk/jscompact.py
@@ -9,11 +9,10 @@ import sys
 import os
 
 def compact_lines(file):
-	p = re.compile("\s+")
 	for line in file:
 		if not re.match("(\s*[/*])|(\s*$)", line):
-			line = p.sub("", line)
-			print line,
+			line = line.strip()
+			print line
 			
 filename = sys.argv[1]
 jsfile = file(filename)        
